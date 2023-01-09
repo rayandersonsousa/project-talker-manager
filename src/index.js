f4aa698a6c546c6695c11edc34035a00e4ffcbaf
Rayanderson Sousa <rayandersonsousa@pop-os.localdomain>
2023-01-09 15:56:35 -0300

    Requisito 01

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+
+const filePath = path.resolve('src', 'talker.json');
 
 const app = express();
 app.use(express.json());
@@ -15,4 +19,13 @@ app.listen(PORT, () => {
   console.log('Online');
 });
 
-// Commit Inicial
+app.get('/talker', async (req, res) => {
+  const response = await fs.readFile(filePath);
+  const responseJSON = JSON.parse(response);
+  if (responseJSON.length === 0) {
+    return res.status(200).send([]);
+  }
+  return res.status(200).json(responseJSON);
+});
+
+module.exports = app;
